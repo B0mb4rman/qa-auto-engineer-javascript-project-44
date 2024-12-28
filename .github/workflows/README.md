a370a3e9f43663ff3d86e4f82cdacb607e9a63c4
evgstepanenko <evgstepanenko@ozon.ru>
2024-12-28 18:13:40 +0200

    project 44, p.6

--- .github/workflows/README.md
+++ .github/workflows/README.md
@@ -5,3 +5,5 @@ After completing all the steps in the project, automatic tests will become avail
 The hexlet-check.yml file is responsible for running these tests - do not delete this file, edit it, or rename the repository.
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/32fe4f5e251c16d32f51/maintainability)](https://codeclimate.com/github/B0mb4rman/qa-auto-engineer-javascript-project-44/maintainability)
+
+[Посмотреть демонстрацию игры "Калькулятор"](https://asciinema.org/connect/807bf2b9-035e-4954-bae5-0a479c2d5fe9)
---
+++ src/games/calc-game.js
@@ -0,0 +1,36 @@
+import { runGame } from '../index.js';
+
+const description = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
+
+const generateRound = () => {
+    const num1 = Math.floor(Math.random() * 100);
+    const num2 = Math.floor(Math.random() * 100);
+    const operator = operators[Math.floor(Math.random() * operators.length)];
+
+    const question = `${num1} ${operator} ${num2}`;
+    let correctAnswer;
+
+    switch (operator) {
+        case '+':
+            correctAnswer = num1 + num2;
+            break;
+        case '-':
+            correctAnswer = num1 - num2;
+            break;
+        case '*':
+            correctAnswer = num1 * num2;
+            break;
+        default:
+            throw new Error(`Unknown operator: ${operator}`);
+    }
+
+    return {
+        question,
+        correctAnswer: String(correctAnswer),
+    };
+};
+
+export const playCalcGame = () => {
+    runGame(description, generateRound);
+};
